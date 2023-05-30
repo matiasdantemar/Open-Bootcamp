7c6ade95ebe7ca1f1da37822ad1266819d168b50
Matias <matiasdantemar@hotmail.com>
2023-05-30 14:56:23 -0300

    add FuncionIva

---
+++ Introducción a la programación/6 Funcion/FuncionIVA/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(getPrecioiva(100));
+    }
+    static double getPrecioiva(double precio){
+        return precio * 1.21;
+    }
+}
