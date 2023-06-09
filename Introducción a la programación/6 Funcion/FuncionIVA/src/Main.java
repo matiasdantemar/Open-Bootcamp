f2a50efc4db0dd5f476012a33e1e12dc90d46fde
Matias <matiasdantemar@hotmail.com>
2023-06-09 04:37:23 -0300

    add array, vectores, InputStream, PrintStream

--- Introducción a la programación/6 Funcion/FuncionIVA/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println(getPrecioiva(100));
-    }
-    static double getPrecioiva(double precio){
-        return precio * 1.21;
-    }
-}
--- Introducción a la programación/9 Interfaces/Intefaz/src/Interfaz/CocheCRUD.java
+++
@@ -1,7 +0,0 @@
-package Interfaz;
-public interface CocheCRUD {
-    void save();
-    void findAll();
-    void delete();
-
-}
